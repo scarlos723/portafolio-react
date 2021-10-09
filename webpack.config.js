e58d04123cc1e3ec1d90cf78232c4b2cd083628d
scarlos <scarlos@unicauca.edu.co>
2021-10-08 19:17:45 -0500

    create webpag babel and src

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    output:{
+        filename: 'app.bundle.js',
+        publicPath: '/'
+    },
+    devServer: {
+        historyApiFallback: {
+          disableDotRule: true
+        }
+    }  ,
+    plugins:[
+        new HtmlWebpackPlugin ({
+            template: "src/index.html"
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  '@babel/preset-env',
+                  '@babel/preset-react'
+                ]
+              }
+            }
+          }
+        ]
+      }
+}
